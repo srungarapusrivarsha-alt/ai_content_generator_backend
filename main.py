e7d072bf4ade93f8a35d4a262980caf603c889ff
srungarapusrivarsha-alt <srungarapusrivarsha@gmail.com>
2026-06-01 16:42:54 +0530

    final backend setup

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ app=FastAPI()
 client=OpenAI(
     api_key=os.getenv("GROQ_API_KEY"),
     base_url="https://api.groq.com/openai/v1")
-
+@app.get("/")
+def home():
+    return{
+        "home":"api running successfully.."
+    }
 @app.post("/generate")
 def generate_content(
     topic:str,
