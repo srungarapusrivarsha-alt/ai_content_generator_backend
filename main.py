a1631d21575f94a095f367aa0d8e9483b8cc5f4e
srungarapusrivarsha-alt <srungarapusrivarsha@gmail.com>
2026-06-01 16:26:54 +0530

    groq api key added

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from openai import OpenAI
 import os
 app=FastAPI()
 client=OpenAI(
-    api_key=os.getenv("groq_key_url"),
+    api_key=os.getenv("GROQ_API_KEY"),
     base_url="https://api.groq.com/openai/v1")
 
 @app.post("/generate")
